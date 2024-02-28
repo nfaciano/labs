1a0189c778171fb2aaac1f59d2dda2420d56ed46
nfaciano <nickfac88@gmail.com>
2024-02-28 13:04:53 -0500

    t

---
+++ js/book-jq.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    // Function to change the opacity of all images and then highlight the clicked one
+    function adjustImageOpacity(exceptId) {
+        $('img').fadeTo('slow', 0.5); // Fade all images to opacity 0.5
+        $(`#${exceptId}`).fadeTo('slow', 1); // Fade the specified image back to opacity 1
+    }
+
+    // Event listener for "Don Quixote" image
+    $('#don-quixote-img').click(function() {
+        adjustImageOpacity('don-quixote-img');
+        $('#details').fadeOut(function() {
+            $(this).load('https://nfaciano.rhody.dev/lab5/data/cervantes-data.html', function() {
+                $(this).fadeIn('slow'); // Ensure you replace the URL with the correct path to your HTML file
+            });
+        });
+    });
+
+    // Event listener for "A Tale of Two Cities" image
+    $('#two-cities-img').click(function() {
+        adjustImageOpacity('two-cities-img');
+        $('#details').fadeOut(function() {
+            $(this).load('https://nfaciano.rhody.dev/lab5/data/dickens-data.html', function() {
+                $(this).fadeIn('slow'); // Ensure you replace the URL with the correct path to your HTML file
+            });
+        });
+    });
+
+    // Event listener for "The Lord of the Rings" image
+    $('#lotr-img').click(function() {
+        adjustImageOpacity('lotr-img');
+        $('#details').fadeOut(function() {
+            $(this).load('https://nfaciano.rhody.dev/lab5/data/tolkien-data.html', function() {
+                $(this).fadeIn('slow'); // Ensure you replace the URL with the correct path to your HTML file
+            });
+        });
+    });
+});
